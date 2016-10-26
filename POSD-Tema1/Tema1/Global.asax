970321544360653fa39103b7196683e11802a474
sinziananicolae <sinzianna.nicolae@gmail.com>
2016-10-26 21:49:14 +0300

    test

---
+++ POSD-Tema1/Tema1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tema1.MvcApplication" Language="C#" %>
---
+++ POSD-Tema1/Tema1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tema1.Startup))]
+namespace Tema1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
